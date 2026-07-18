5b97ea9424e8481221b35c1864ff580b7c272a17
Ayshen2006 <13561@holbertonstudents.com>
2026-07-18 16:13:07 +0400

    fixedd

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -46,8 +46,7 @@ class Server:
 
         return dataset[start:end]
 
-    def get_hyper(self, page: int = 1,
-                  page_size: int = 10) -> Dict:
+    def get_hyper(self, page: int = 1,page_size: int = 10) -> Dict:
         """Return hypermedia pagination information."""
         data = self.get_page(page, page_size)
         total_pages = math.ceil(len(self.dataset()) / page_size)
