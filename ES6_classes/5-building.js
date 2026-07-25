6ef26f24a4c8c898f2c6b78fd18c835dc3a6361a
Ayshan <13561@holbertonstudents.com>
2026-07-25 04:55:29 -0700

    ok

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+
+    if (
+      this.constructor !== Building
+      && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    // This method should be overridden by subclasses.
+  }
+}
