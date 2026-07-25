5a325e79cbac86e5bdf2785e488710be81a7703f
Ayshan <13561@holbertonstudents.com>
2026-07-25 05:40:45 -0700

    10 loop is fixed

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,1 +1,9 @@
-export default function appendToEachArrayValue(array, appendString) { for (const [idx, value] of array.entries()) array[idx] = appendString + value; return array; }
+export default function appendToEachArrayValue(array, appendString) {
+    let index = 0;
+
+    for (const value of array) {
+        array[index] = appendString + value;
+        index++;
+    }
+    return array;
+}
