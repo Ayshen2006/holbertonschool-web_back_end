4ad5497554e80b435397e888c08650a07400129a
Ayshan <13561@holbertonstudents.com>
2026-08-08 10:48:36 -0700

    5 is ready

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const fs = require('fs');
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!\n');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+
+    const database = process.argv[2];
+
+    fs.readFile(database, 'utf-8', (error, data) => {
+      if (error) {
+        res.end('Cannot load the database\n');
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+
+      res.write(`Number of students: ${students.length}\n`);
+
+      const fields = {};
+
+      students.forEach((student) => {
+        const [firstname, lastname, age, field] = student.split(',');
+
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+
+        fields[field].push(firstname);
+      });
+
+      Object.keys(fields).forEach((field) => {
+        res.write(
+          `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`,
+        );
+      });
+
+      res.end();
+    });
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
