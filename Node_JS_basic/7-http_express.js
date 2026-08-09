1d724ee77fe544a2d505f64ac17ec9b085e27ecf
Ayshan <13561@holbertonstudents.com>
2026-08-08 22:54:19 -0700

    7 is fixed

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -4,41 +4,43 @@ const fs = require('fs');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.type('text').send('Hello Holberton School!\n');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
   const database = process.argv[2];
 
-  fs.readFile(database, 'utf-8', (error, data) => {
-    if (error) {
-      res.status(500).send('Cannot load the database\n');
+  fs.readFile(database, 'utf8', (err, data) => {
+    if (err) {
+      res.send('This is the list of our students\nCannot load the database');
       return;
     }
 
     const lines = data.split('\n').filter((line) => line.trim() !== '');
     const students = lines.slice(1);
 
-    let output = 'This is the list of our students\n';
-    output += `Number of students: ${students.length}\n`;
+    let result = 'This is the list of our students\n';
+    result += `Number of students: ${students.length}\n`;
 
     const fields = {};
 
-    students.forEach((student) => {
-      const [firstname, , , field] = student.split(',');
+    for (const student of students) {
+      const values = student.split(',');
+      const firstname = values[0];
+      const field = values[3];
 
       if (!fields[field]) {
         fields[field] = [];
       }
 
       fields[field].push(firstname);
-    });
+    }
 
-    Object.keys(fields).forEach((field) => {
-      output += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
-    });
+    for (const field of Object.keys(fields)) {
+      result += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+    }
 
-    res.send(output);
+    res.send(result);
   });
 });
 
